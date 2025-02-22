d49fff3af1acfbb9ffe1dda1180eec717a2c72e6
Titus_Adesina_TU <87777425+Kuroganeda1@users.noreply.github.com>
2025-02-22 07:40:00 -0500

    Add files via upload

--- script.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbarMenu');
-
-menu.addEventListener(`click`, function() {
-  menu.classList.toggle(`is-active`);
-  menuLinks.classList.toggle(`active`);
-});
-
